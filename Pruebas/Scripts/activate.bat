30508463a6ee5a49e6e5c49b20be58fe330b71ba
Keyngell <mickey97r02s18@gmail.com>
2022-09-13 22:11:26 -0500

    Ya no sube el entorno virtual e.e

--- Pruebas/Scripts/activate.bat
+++ Pruebas/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\JetBrains\VHBA\pruebas\Pruebas
+set VIRTUAL_ENV=D:\Mouse\Documents\Codes\Private\GitProjects\esp\Pruebas
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,6 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(Pruebas) 
 
 :END
 if defined _OLD_CODEPAGE (
