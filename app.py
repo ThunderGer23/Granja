54a2a75daa56144a80801bb418e18e96ab765862
Keyngell <mickey97r02s18@gmail.com>
2022-09-21 13:16:53 -0500

    Se borro la carpeta de pruebas

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ import time
 
 p = Pin(2, Pin.OUT)
 
-def toggle(max):
+def toggle(max,t=1):
     lap = 0
 
     while lap < max:
         p.value(1)
-        time.sleep(1)
+        time.sleep(t)
         p.value(0)
-        time.sleep(1)
+        time.sleep(t)
         lap += 1
 
-toggle(10)
+toggle(10,.5)
