aaceffc8b582cee251f4a1ce34fc1e5da2806d87
ThunderGer <jaames11@hotmail.com>
2023-01-07 15:17:21 -0600

    PWM

--- app.py
+++ app.py
@@ -1,16 +1,48 @@
-from machine import Pin
-import time
+from machine import Pin, PWM, Timer
+from time import sleep_ms
+
+def calperiod(grad):
+    return (grad * 100)/10800
 
 p = Pin(2, Pin.OUT)
 
-def toggle(max,t=1):
-    lap = 0
+frecuency = 500
+duty = 0
+dutyOff = 1023
+pwm12 = PWM(Pin(12))
+pwm12 = PWM(Pin(13))
+pwm12 = PWM(Pin(14))
 
-    while lap < max:
-        p.value(1)
-        time.sleep(t)
-        p.value(0)
-        time.sleep(t)
-        lap += 1
+pwm12.freq(frecuency)
+pwm12.duty(dutyOff)
 
-toggle(10,.5)
+#variables
+motor = 1
+grad = 35
+period1 = calperiod(grad)
+period2 = calperiod(grad)
+period3 = calperiod(grad)
+period4 = calperiod(grad)
+period5 = calperiod(grad)
+
+
+#set timer
+tim1 = Timer(1)
+tim2 = Timer(1)
+tim3 = Timer(1)
+p.value(1)
+tim1.init(period=period, mode=Timer.PERIODIC, callback=lambda t:setLed())
+
+def setLed():
+
+    duty -= 10
+    sleep_ms(3)
+    if duty < 0:
+        duty = dutyOff
+        motor += 1
+        if motor > 3:
+            motor = 1
+
+    if motor == 1:
+        pwm12.duty(dutyOff)
+    p.value(0)
