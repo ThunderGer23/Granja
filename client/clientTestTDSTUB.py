53d02ea6e2887e4e0d734957e205112e1f764354
Keyngell <mickey97r02s18@gmail.com>
2023-02-02 01:40:38 -0600

    Se integró la funcionalidad de mandar la info en formato de json como string plano :'v

--- client/clientTestTDSTUB.py
+++ client/clientTestTDSTUB.py
@@ -3,9 +3,9 @@ from machine import Pin, ADC
 from machine.ADC import ATTN_0DB, WIDTH_12BIT
 from time import sleep_ms as slms
 from alerts.messages import MessageAlertValues
-from accesscodes.keys import passwordESPServ as passw
-from accesscodes.keys import nameModule as mod
-from accesscodes.connectTo import internet
+from access.keys import passwordESPServ as passw
+from access.keys import nameModule as mod
+from access.connectTo import internet
 
 # Setting the pins for the sensors.
 pinSensorTDS = 39
@@ -27,7 +27,7 @@ bTem = 0
 TEMP = 25
 SCOUNT = 30
 VREF = 3.3
-SENSERATE = 40 #ms
+SENSE_RATE = 40 #ms
 
 # A lambda function that is used to read the value of the Turbidity sensor.
 readSenseTUB = lambda : sensTUB.read() * 0.000855
@@ -56,7 +56,7 @@ def readTDSValue():
     :return: The TDS value is being returned.
     """
     if (SCOUNT >= 0):
-        slms(SENSERATE)
+        slms(SENSE_RATE)
         analogBuffer.append(sensTDS.read())
         SCOUNT -= 1
         # ? Return analogBuffer to empty after SCOUNT equal to 0
@@ -69,11 +69,11 @@ def readTDSValue():
         # ! If the temperature value is greater or lower, it must be adjusted to compensate for the sensor
         compensationCoefficient = 1+float(0.02*(TEMP-25));
         compensationVoltage = averageVoltage/compensationCoefficient
-        tdsValue=(133.42*compensationVoltage*compensationVoltage*compensationVoltage - 255.86*compensationVoltage*compensationVoltage + 857.39*compensationVoltage)*0.5;
+        tdsValue=(133.42*(compensationVoltage**3) - 255.86*(compensationVoltage**2) + 857.39*compensationVoltage)*0.5;
         # * Additionally you can return averageVoltage to have a reference
         return tdsValue # or {averageVoltage, tdsValue}
 
-# ! def initSensors():
+def initSensors():
     #TODO: Evaluate the return values based on the parameters that we require to
     #TODO: Promote the life of marine species and if those values get out of rank
     #TODO: Execute While
@@ -87,25 +87,25 @@ def readTDSValue():
     # *      s.sendall(message)
     # *      print(str(s.recv(4096),'utf-8'))
     # *      s.close()
-    # ? try:
-    # ?     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    # ?     addr = ("192.168.4.1",80)
-    # ?     sensTDS = readTDSValue()
-    # ?     sensTUB = readSenseTUB()
-    # ?     slms(SENSERATE)
-    #       # TODO: Here the conditional to evaluate the values and if we meet the connection
-    # ?     s.connect(addr)
-    # ?     message = bytes(input(MessageAlertValues + str(sensTDS) + str(sensTUB)),'utf-8')
-    # ?     s.sendall(message)
-    # ? except Exception as e:
-    # ?     print(e,'!!!')
-    # ?     pass
-    #       # TODO: Here you could also validate whether the message is sent correctly asking
-    #       # TODO: for the value of return of str(s.recv(4096),'utf-8'), if the message is not
-    #       # TODO: make success to repeat data 3 times
-    # ? finally:
-    # ?     s.close()
-    # ? return
+    try:
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        addr = ("192.168.4.1",80)
+        sensTDS = readTDSValue()
+        sensTUB = readSenseTUB()
+        slms(SENSE_RATE)
+        # TODO: Here the conditional to evaluate the values and if we meet the connection
+        s.connect(addr)
+        message = bytes(f'"MAV":"{MessageAlertValues}",  "sensTDS":"{str(sensTDS)}",  "sensTUB":"{str(sensTUB)}"','utf-8')
+        s.sendall(message)
+    except Exception as e:
+        print(e,'!!!')
+        pass
+        # TODO: Here you could also validate whether the message is sent correctly asking
+        # TODO: for the value of return of str(s.recv(4096),'utf-8'), if the message is not
+        # TODO: make success to repeat data 3 times
+    finally:
+        s.close()
+    return
 
 # ! internet(mod, passw)
 # ! initSensors()
